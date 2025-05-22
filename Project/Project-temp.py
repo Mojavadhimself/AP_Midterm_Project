305862acaca717f2ba408fa4790062490b4ac4d7
Mohammadjavad Hosseiny <smojavad.first@gmail.com>
2025-05-22 17:32:32 +0330

    File and FileSystem classes completed

--- Project/Project-temp.py
+++ Project/Project-temp.py
@@ -16,8 +16,35 @@ class FileSystem:
         self.path = '/'
         self.directories = {'/': Directory('/')}
 
-    def get_path(self):
-        pass
+    def get_path(self, path):
+        if path[0] == '/':
+            return self.absolute_path(path)
+        else:
+            return self.relative_path(path)
+
+    def absolute_path(self, path):
+        if path == '/':
+            return self.directories['/']
+
+        parts = path.split("/")
+        parts.remove("")
+
+        current = self.root
+
+        for part in parts:
+            found = False
+            for directory in current.child:
+                if directory.name == part:
+                    current = directory
+                    found = True
+                    break
+            if not found:
+                print("Path not found")
+                return None
+        return current
+
+    def relative_path(self, path):
+        return self.absolute_path(self.path.rstrip('/') + '/' + path)
 
 
 class Directory:
@@ -54,19 +81,44 @@ class File:
         self.parent = parent
 
     def newfiletxt(self):
-        pass
+        print("Enter your text: (/e means done)")
+        text = []
+        while True:
+            line = input()
+            if line == '/e':
+                break
+            text.append(line)
+        self.data = text
+        print(f"{self.name} overwritten")
 
     def appendtxt(self):
-        pass
+        print("Enter your text to append: (/e means done)")
+        text = []
+        while True:
+            line = input()
+            if line == '/e':
+                break
+            text.append(line)
+        self.data.extend(text)
+        print(f"{self.name} overwritten")
 
     def editline(self, line, text):
-        pass
+        if 0 <= line < len(self.data):
+            self.data[line] = text
+            print(f"{self.name} overwritten")
+        else:
+            print(f"{self.name} have not this line")
 
     def delline(self, line):
-        pass
+        if 0 <= line < len(self.data):
+            self.data.pop(line)
+            print(f"{self.name} overwritten")
+        else:
+            print(f"{self.name} have not this line")
 
     def cat(self):
-        pass
+        for line in self.data:
+            print(line)
 
 
 class CommandParser:
@@ -102,3 +154,13 @@ class CommandParser:
             pass
         elif self.cmd == 'ls':
             pass
+
+
+if __name__ == '__main__':
+    filesystem = FileSystem('/')
+
+    while True:
+        print(filesystem.path)
+        command = input()
+        parser = CommandParser(command)
+        parser.handle_command()
